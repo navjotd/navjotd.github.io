68dd78ae08a531753127a018f586983cdfca2c6d
nav <navjot.dznr@gmail.com>
2015-06-15 19:16:14 -0400

    maison

---
+++ maison/scripts/main.js
@@ -0,0 +1,21 @@
+var s = skrollr.init();
+
+$(document).ready(function(){
+    site.resize();
+    $(window).resize(function(){
+        site.resize();
+    });
+});
+
+var site = {
+    resize: function(){
+        var Imgposition = $('.feature').position();
+        var width = $('#logo').width();
+        var val2 = Imgposition.left/2 - 97;
+        var ans = Imgposition.left/2 - width/2
+        var value = (ans >= 10)? ans: 10;
+        $('.left').css('left', val2);
+        $('.right').css('right', val2);
+        $('#logo').css('left', value);
+    }
+};
